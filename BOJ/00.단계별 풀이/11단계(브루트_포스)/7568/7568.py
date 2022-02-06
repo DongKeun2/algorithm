3fb1889627613d1aab16f1c013c7e5417aa6be57
DongKeun2 <ehdrms121@ajou.ac.kr>
2022-02-06 20:10:46 +0900

    Update BOJ_7568

--- BOJ/00.단계별 풀이/11단계(브루트_포스)/7568/7568.py
+++ BOJ/00.단계별 풀이/11단계(브루트_포스)/7568/7568.py
@@ -2,6 +2,18 @@
 
 N = int(input())
 
-list_x = []
+xl = []
 for _ in range(N):
     X = list(map(int, input().split()))
+    xl.append(X)
+
+result = []
+
+for i in xl:
+    cnt = 1
+    for j in xl:
+        if i[0] < j[0] and i[1] < j[1]:
+            cnt += 1
+    result.append(cnt)
+
+print(' '.join(str(x) for x in result))
