faf54a7b4df24617699372a13aef7e52b7504892
DongKeun2 <ehdrms121@ajou.ac.kr>
2022-02-08 23:33:59 +0900

    Add BOJ_1918

--- BOJ/01.Data_Structures/1918/1918.py
+++ BOJ/01.Data_Structures/1918/1918.py
@@ -1,2 +1,40 @@
 # 후위 표기식
 
+word = input()
+
+result = ''
+st = []
+for s in word:
+    if 65 <= ord(s) <= 90:
+        result += s
+    elif s == '(':
+        st.append(s)
+    elif (s == '+') or (s == '-'):
+        if st:
+            while st and ((st[-1] == '*') or (st[-1] == '+') or (st[-1] == '/') or (st[-1] == '-')):
+                result += st.pop()
+            st.append(s)
+        else:
+            st.append(s)
+    elif (s == '*') or (s == '/'):
+        if st:
+            while st and ((st[-1] == '*') or (st[-1] =='/')):
+                result += st.pop()
+            st.append(s)
+        else:
+            st.append(s)
+    
+    else:
+        while st[-1] != '(':
+            result += st.pop()
+        st.pop()
+
+for x in st[::-1]:
+    result += x
+
+print(result)
+
+
+
+
+
