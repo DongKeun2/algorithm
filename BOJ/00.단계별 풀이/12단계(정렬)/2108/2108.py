5ac63af3f769a681ed1483158c399d2745681e70
DongKeun2 <ehdrms121@ajou.ac.kr>
2022-02-12 02:39:25 +0900

    Add BOJ_2108

--- BOJ/00.단계별 풀이/12단계(정렬)/2108/2108.py
+++ BOJ/00.단계별 풀이/12단계(정렬)/2108/2108.py
@@ -1,29 +1,30 @@
 # 통계학
+import sys
+input = sys.stdin.readline
 
 N = int(input())
 
-n_list = []
+nl = []
 for _ in range(N):
-    n_list.append(int(input()))
+    nl.append(int(input()))
 
-print(round(sum(n_list)/len(n_list)))
-print(sorted(n_list)[int(len(n_list)/2)])
+print(round(sum(nl)/len(nl)))
+print(sorted(nl)[int(len(nl)/2)])
 
-max_list = []
-max_cnt = 0
-n_set = set(n_list)
-for i in n_set:
-    if max_cnt < n_list.count(i):
-        max_cnt = n_list.count(i)
-        max_list = [i]
-    elif max_cnt == n_list.count(i):
-        max_list.append(i)
+cl = [0] * 8001
 
-if len(max_list) == 1:
-    print(max_list[0])
-else:
-    max_list.sort()
-    print(max_list[1])
+for n in nl:
+    cl[n+4000] += 1
 
+mc = max(cl)
+cnt = 0
+for i in range(len(cl)):
+    if cl[i] == mc:
+        cnt += 1
+        mi = i
+        if cnt >= 2:
+            break
+    
+print(mi-4000)
 
-print(max(n_list)-min(n_list))
+print(max(nl)-min(nl))
