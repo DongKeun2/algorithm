837543e04fd3a2022d49cb40dea80512220c3aed
DongKeun2 <ehdrms121@ajou.ac.kr>
2022-02-16 01:09:00 +0900

    Modify BOJ_9465_condition

--- BOJ/03.Dynamic_Programming/9465/9465.py
+++ BOJ/03.Dynamic_Programming/9465/9465.py
@@ -6,11 +6,12 @@ for _ in range(T):
     arr = [list(map(int, input().split())) for _ in range(2)]
     dp = [[arr[0][i], arr[1][i]] for i in range(n)]
 
-    dp[1][0] += dp[0][1]
-    dp[1][1] += dp[0][0] 
-    for i in range(2, n):
-        dp[i][0] = max(dp[i-1][1] + dp[i][0], dp[i-2][1] + dp[i][0])
-        dp[i][1] = max(dp[i-1][0] + dp[i][1], dp[i-2][0] + dp[i][1])
+    if n >= 2:
+        dp[1][0] += dp[0][1]
+        dp[1][1] += dp[0][0] 
+        for i in range(2, n):
+            dp[i][0] = max(dp[i-1][1] + dp[i][0], dp[i-2][1] + dp[i][0])
+            dp[i][1] = max(dp[i-1][0] + dp[i][1], dp[i-2][0] + dp[i][1])
 
     result = max(dp[n-1][0], dp[n-1][1])
 
