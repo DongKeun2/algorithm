6ee69b015767839ee4e8da0a319e28c905d3f7f4
DongKeun2 <ehdrms121@ajou.ac.kr>
2022-03-04 21:04:56 +0900

    Add BOJ_15649

---
+++ BOJ/12.Backtracking/15649.py
@@ -0,0 +1,20 @@
+# N과 M(1)
+
+def sol(lst):
+    if len(result) == M:
+        print(*result)
+    else:
+        for i in range(N):
+            if v[i] == 0:
+                v[i] = 1
+                result.append(lst[i])
+                sol(lst)
+                result.pop()
+                v[i] = 0
+        return
+
+N, M = map(int, input().split())
+lst = [i for i in range(1, N+1)]
+result = []
+v = [0 for _ in range(N)]
+sol(lst)
