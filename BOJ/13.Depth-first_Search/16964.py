e015dea521bc70539d42dce1f98ff5544db21241
DongKeun2 <ehdrms121@ajou.ac.kr>
2022-03-28 11:00:51 +0900

    Add BOJ_16964

---
+++ BOJ/13.Depth-first_Search/16964.py
@@ -0,0 +1,32 @@
+# DFS 스페셜 저지
+
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+arr = [[] for _ in range(N+1)]
+for _ in range(N-1):
+    a, b = map(int, input().split())
+    arr[a].append(b)
+    arr[b].append(a)
+arr[0].append(1)
+
+lst = list(map(int, input().split()))
+
+idx = 0
+st = [0]
+result = 1
+while True:
+    if st == []:
+        result = 0
+        break
+    if idx >= N:
+        break
+
+    if lst[idx] in arr[st[-1]]:
+        st.append(lst[idx])
+        idx += 1
+    else:
+        st.pop()
+
+print(result)
