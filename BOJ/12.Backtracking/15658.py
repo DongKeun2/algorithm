1b4bc267a13133052658f31d8599c4c31ed67a61
DongKeun2 <ehdrms121@ajou.ac.kr>
2022-04-08 00:42:47 +0900

    Add BOJ_15658

---
+++ BOJ/12.Backtracking/15658.py
@@ -0,0 +1,40 @@
+# 연산자 끼워넣기(2)
+# 구현, 브루트 포스, 백트래킹
+
+# 연산자 끼워넣기(14888)와 같은 풀이
+def sol(idx, num, p, s, m, d):
+    global result1, result2
+
+    if idx >= N:
+        if result1 < num:
+            result1 = num
+        if result2 > num:
+            result2 = num
+        return
+
+    if p > 0:
+        sol(idx+1, num+lst[idx], p-1, s, m, d)
+
+    if s > 0:
+        sol(idx+1, num-lst[idx], p, s-1, m, d)
+
+    if m > 0:
+        sol(idx+1, num*lst[idx], p, s, m-1, d)
+
+    if d > 0:
+        if num >= 0:
+            sol(idx+1, num//lst[idx], p, s, m, d-1)
+        else:
+            sol(idx+1, -(abs(num)//lst[idx]), p, s, m, d-1)
+
+
+N = int(input())
+lst = list(map(int, input().split()))
+op = list(map(int, input().split()))
+
+result1 = -10**9
+result2 = 10**9
+sol(1, lst[0], op[0], op[1], op[2], op[3])
+
+print(result1)
+print(result2)
