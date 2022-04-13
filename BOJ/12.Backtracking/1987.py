95270ef68a921f8714f562319aa09027ec18bb09
DongKeun2 <ehdrms121@ajou.ac.kr>
2022-04-14 00:27:35 +0900

    Add BOJ_1987

--- BOJ/12.Backtracking/1987.py
+++ BOJ/12.Backtracking/1987.py
@@ -2,14 +2,19 @@
 # 그래프이론, 그래프탐색, 깊이우선탐색, 백트래킹
 # pypy통과
 
+# dfs
+# tmp의 길이 cnt (이동거리)
 def sol(si, sj, cnt):
     global result
 
+    # 4방향 탐색
     for d in range(4):
         ni = si + di[d]
         nj = sj + dj[d]
+        # 방문하지 않았다면 tmp에 추가
         if 0<=ni<R and 0<=nj<C and vst[ni][nj] == 0:
             tmp.add(arr[ni][nj])
+            # tmp의 길이가 늘어났다면 중복 x 이동가능
             if len(tmp) == cnt+1:
                 vst[ni][nj] = 1
                 sol(ni, nj, cnt+1)
