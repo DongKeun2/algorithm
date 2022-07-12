8d5c8f20889ba818e521059c61eb768f38f5b588
DongKeun2 <ehdrms121@ajou.ac.kr>
2022-07-12 23:33:05 +0900

    Add BOJ_2293

---
+++ BOJ/03.Dynamic_Programming/2293.py
@@ -0,0 +1,15 @@
+# 동전 1
+
+n, k = map(int, input().split())
+
+# 동전 종류
+lst = [int(input()) for _ in range(n)]
+
+dp = [0] * (k+1)
+dp[0] = 1
+for i in range(n):
+    for j in range(lst[i], k+1):
+        if j - lst[i] >= 0:
+            dp[j] += dp[j-lst[i]]
+
+print(dp[k])
