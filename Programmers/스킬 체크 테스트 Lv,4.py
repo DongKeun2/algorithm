6901c3c64c2671159a211b8bfbe2694f449243e2
dongkeun2 <ehdrms121@ajou.ac.kr>
2023-01-23 15:38:03 +0900

    Add programmers lv4

---
+++ Programmers/스킬 체크 테스트 Lv,4.py
@@ -0,0 +1,29 @@
+# DP로 풀이
+def solution(money):
+    money = [0] + money
+    n = len(money)
+
+    # 집이 4개 이상이면
+    if n > 4:
+        dp = [[0, 0] for _ in range(n)]
+        
+        # 첫 집을 털거나 안 턴 경우
+        dp[1][0] = money[1]
+        dp[1][1] = 0
+
+        # 이번 집을 안털거나, 턴 경우 중 큰 값 저장
+        for i in range(2, len(money)-1):
+            dp[i][0] = max(dp[i-1][0], dp[i-2][0] + money[i])
+            dp[i][1] = max(dp[i-1][1], dp[i-2][1] + money[i])
+        
+        # 첫 집을 털었으면 마지막 집은 털지 않는다. 
+        dp[-1][0] = dp[-2][0]
+        dp[-1][1] = max(dp[-3][1] + money[-1], dp[-2][1])
+
+        answer = max(dp[-1])
+    
+    # 집이 3개면 한 집만 가능
+    else:
+        answer = max(money)
+
+    return answer
