952aaccaa73a53ee639d54b1b108cb23702f56fa
dongkeun2 <ehdrms121@ajou.ac.kr>
2023-04-26 13:59:03 +0900

    fix: 3차 압축 해설

--- Programmers/Lv2/[3차] 압축.py
+++ Programmers/Lv2/[3차] 압축.py
@@ -2,10 +2,10 @@
 # 2018 KAKAO BLIND RECRUITMENT
 
 # dct에 문자열 : 번호 등록
-# A~Z까지 1~27로 등록
+# A~Z까지 1~26로 등록
 # 0번 인덱스부터 다음 문자열과 합친 문자가 dct에 존재한다면 idx증가
 # 없다면 해당 문자열의 dct 값을 answer에 저장 후 인덱스 증가
-# 다음 문자열을 28부터 순서대로 등록 
+# 다음 문자열을 27부터 순서대로 등록 
 def solution(msg):
     dct = {}
     cnt = 1
